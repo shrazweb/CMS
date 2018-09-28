bfcd6255d0d8f25e1b441e41c0853edeaabfa074
Shrazweb Yazılım ve Tasarım <shrazweb@hotmail.com>
2018-09-28 14:57:27 +0300

    ürün listesi için html şablonu oluşturuldu

--- application/views/product_v/list/content.php
+++ application/views/product_v/list/content.php
@@ -1,1 +1,31 @@
-Boş Ürünler
+<div class="row">
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <h4 class="m-b-lg">Ürün Listesi</h4>
+            <table class="table table-striped table-hover">
+                <thead>
+                <th>#id</th>
+                <th>url</th>
+                <th>Başlık</th>
+                <th>Açıklama</th>
+                <th>Durumu</th>
+                <th>İşlemler</th>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>No</td>
+                    <td>monitor-askisi</td>
+                    <td>Monitör Askısı</td>
+                    <td>360 derece dönebilen monitör askısı</td>
+                    <td>1/0</td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                        <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-edit"></i> Düzenle</a>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
